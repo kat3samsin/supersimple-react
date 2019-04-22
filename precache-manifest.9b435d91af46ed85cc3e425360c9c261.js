fc1e80d536dd7890faaeabdb3c0120b6947abc59
katre <kat3samsin@gmail.com>
2019-04-22 14:08:25 +0800

    Updates

---
+++ precache-manifest.9b435d91af46ed85cc3e425360c9c261.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd5b069aa5aa629f1e2d",
+    "url": "/supersimple-react/static/js/runtime~main.eb60407a.js"
+  },
+  {
+    "revision": "19b543dea8673e54f979",
+    "url": "/supersimple-react/static/js/main.d1c72b32.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/supersimple-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "19b543dea8673e54f979",
+    "url": "/supersimple-react/static/css/main.69e76c4c.chunk.css"
+  },
+  {
+    "revision": "70ff986492ecb9a6b6112b591470ef36",
+    "url": "/supersimple-react/index.html"
+  }
+];
